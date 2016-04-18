3cfb857bcaa62cf58069dff0981edc7088c18ee5
Laura Chen <jhclaura@gmail.com>
2016-04-18 17:42:40 -0400

    update pointer locker and fix user-select bug

--- js/script_vSimple.js
+++ js/script_vSimple.js
@@ -21,50 +21,47 @@ WebVRConfig = {
 				'mozPointerLockElement' in document || 
 				'webkitPointerLockElement' in document;
 
-	if ( havePointerLock ) {
-		blocker = document.getElementById('blocker');
-		instructions = document.getElementById('instructions');
+	if( !isMobile ) {
+		if ( havePointerLock ) {
+			blocker = document.getElementById('blocker');
+			instructions = document.getElementById('instructions');
+		
+			var pointerlockchange = function ( event ) {
 
-		var pointerlockchange = function ( event ) {
+				if ( document.pointerLockElement === element || document.mozPointerLockElement === element || document.webkitPointerLockElement === element ) {
+					console.log("enable pointerControls");
 
-			if ( document.pointerLockElement === element || document.mozPointerLockElement === element || document.webkitPointerLockElement === element ) {
-				console.log("enable pointerControls");
+					controls.enabled = true;
+					blocker.style.display = 'none';
 
-				controls.enabled = true;
-				blocker.style.display = 'none';
+				} else {
 
-			} else {
+					controls.enabled = false;
+					blocker.style.display = '-webkit-box';
+					blocker.style.display = '-moz-box';
+					blocker.style.display = 'box';
 
-				controls.enabled = false;
-				blocker.style.display = '-webkit-box';
-				blocker.style.display = '-moz-box';
-				blocker.style.display = 'box';
+					instructions.style.display = '';
+				}
+			}
 
+			var pointerlockerror = function(event){
 				instructions.style.display = '';
 			}
-		}
 
-		var pointerlockerror = function(event){
-			instructions.style.display = '';
-		}
+			// Hook pointer lock state change events
+			document.addEventListener( 'pointerlockchange', pointerlockchange, false );
+			document.addEventListener( 'mozpointerlockchange', pointerlockchange, false );
+			document.addEventListener( 'webkitpointerlockchange', pointerlockchange, false );
 
-		// Hook pointer lock state change events
-		document.addEventListener( 'pointerlockchange', pointerlockchange, false );
-		document.addEventListener( 'mozpointerlockchange', pointerlockchange, false );
-		document.addEventListener( 'webkitpointerlockchange', pointerlockchange, false );
+			document.addEventListener( 'pointerlockerror', pointerlockerror, false );
+			document.addEventListener( 'mozpointerlockerror', pointerlockerror, false );
+			document.addEventListener( 'webkitpointerlockerror', pointerlockerror, false );
 
-		document.addEventListener( 'pointerlockerror', pointerlockerror, false );
-		document.addEventListener( 'mozpointerlockerror', pointerlockerror, false );
-		document.addEventListener( 'webkitpointerlockerror', pointerlockerror, false );
-
-		if(isMobile) {
-			console.log("isTouchDevice");
-			// instructions.addEventListener( 'touchend', funToCall, false );
-		} else {
 			instructions.addEventListener( 'click', funToCall, false );
+		} else {
+				instructions.innerHTML = 'Your browser doesn\'t seem to support Pointer Lock API';
 		}
-	} else {
-		instructions.innerHTML = 'Your browser doesn\'t seem to support Pointer Lock API';
 	}
 
 	function funToCall(event){
